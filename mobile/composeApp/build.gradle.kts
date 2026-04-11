9179ebe0bfbcf6a152309a8fd42287a86bc4e6c0
saeed heidari <s.heidari@jabama.ir>
2026-04-11 14:14:26 +0330

    build: configure release build type to use debug signing config

--- mobile/composeApp/build.gradle.kts
+++ mobile/composeApp/build.gradle.kts
@@ -115,6 +115,7 @@ android {
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
 
