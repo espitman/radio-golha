dc8121263a7399f2b1b9cd55955f4df5139dbfe4
saeed heidari <s.heidari@jabama.ir>
2026-04-14 20:25:31 +0330

    feat: implement splash screen activity and define custom application themes

---
+++ mobile/composeApp/src/androidMain/kotlin/com/radiogolha/mobile/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.radiogolha.mobile
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class SplashActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+    }
+}
